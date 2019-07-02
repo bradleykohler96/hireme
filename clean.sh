da4ac566da9b2690b423bc02c6e01d5808f631c1
bradleykohler96 <bradleykohler96@gmail.com>
2019-07-02 01:05:02 -0400

    Mix between tabs and spaces.

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@ shopt -s nullglob
 array=(*.tex)
 for i in "${array[@]}"
 do
-	echo "file found $i"
+    echo "file found $i"
     basename $i
     echo "deleting files generated from xelatex"
     rm -f ${i%.tex}.aux
