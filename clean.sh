048334313c835a895f009ae60c21868753266f7f
bradleykohler96 <bradleykohler96@gmail.com>
2019-07-02 01:02:22 -0400

    Modified build system.

---
+++ clean.sh
@@ -0,0 +1,14 @@
+shopt -s nullglob
+array=(*.tex)
+for i in "${array[@]}"
+do
+	echo "file found $i"
+    basename $i
+    echo "deleting files generated from xelatex"
+    rm -f ${i%.tex}.aux
+    rm -f ${i%.tex}.log
+    rm -f ${i%.tex}.out
+    rm -f ${i%.tex}.pdf
+    echo "deleting ${i%.tex}.jpg image"
+    rm -f images/${i%.tex}[a-zA-Z0-9.-]*.jpg
+done
