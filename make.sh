c4ea0d8404f8f83c60050671421243ebdb15960f
Bradley Kohler <kohlerba@mcmaster.ca>
2019-07-29 23:35:13 -0400

    TeXLive does not allow include-directory and MikTeX is only for Windows.

--- make.sh
+++ make.sh
@@ -9,32 +9,44 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
     # POSIX compatibility layer and Linux environment emulation for Windows
     GS=gswin64c.exe
 elif [[ "$OSTYPE" == "msys" ]]; then
-    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    # lightweight shell and GNU utilities compiled for Windows (part of MinGW)
     GS=gswin64c.exe
 else
-    # Unknown.
+    # unknown
     echo "no known operating system"
-    GS=gs
+    GS=
 fi
 
 if ! type "$GS" > /dev/null; then
     echo "ghostscript not installed"
 fi
 
-SOURCE_DIR="src"
-BUILD_DIR="build"
-IMAGE_DIR="images"
+# the source directory
+SOURCE_DIR="./src"
+# the build directory relative to the project source
+BUILD_DIR="../build"
+# the image directory relative to the project source
+IMAGE_DIR="../images"
+
+echo "changing directory to ./$SOURCE_DIR"
+cd "$SOURCE_DIR"
 
 shopt -s nullglob
-array=($SOURCE_DIR/*.tex)
+# make an array of source files ending with .tex
+array=(./*.tex)
 for i in "${array[@]}"
 do
     echo "file found $i"
-    echo "creating files generated from pdflatex"
-    xelatex -interaction=nonstopmode -halt-on-error -file-line-error -quiet -include-directory=$SOURCE_DIR -output-directory=$BUILD_DIR $i
+    echo "creating files generated from xelatex"
+    # crate pdfs with xelatex
+    xelatex -interaction=nonstopmode -halt-on-error -file-line-error -output-directory=$BUILD_DIR $i
+    # check if ghostscript is availible
     if command -v $GS > /dev/null; then
+    # find the file name
     filename=$(basename $i)
+    # echo the file name ending in .jpg
     echo "creating ${filename%.tex}.jpg image for readme"
+    # create the .jpg image using ghostscript
     $GS -q -dNOPAUSE -sDEVICE=jpeg -r400 -dJPEGQ=60 -sOutputFile=$IMAGE_DIR/${filename%.tex}-%03d.jpg $BUILD_DIR/${filename%.tex}.pdf -dBATCH
     fi
 done
